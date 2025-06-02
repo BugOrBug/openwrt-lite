1cdbfd86993e1674ee82d3d2d8b5bd53cf48e05d
BugOrBug <BugOrBug>
2025-06-02 19:37:48 +0800

    update

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -16,6 +16,7 @@ cp -a ../master/packages/libs/boost feeds/packages/libs/boost
 
 # default settings
 git clone https://$github/pmkol/default-settings package/new/default-settings -b lite --depth 1
+[ "$OPKG_PROXY" = "y" ] && sed -i 's#openwrt-lite.pages.dev/openwrt#git.apad.pro/https://raw.githubusercontent.com/pmkol/openwrt-feeds/opkg-repo/openwrt#g' package/new/default-settings/default/zzz-default-settings
 
 # luci - replace version with build date
 [ "$NO_APPS" != "y" ] && sed -i '/# timezone/i sed -i "s/\\(DISTRIB_DESCRIPTION=\\).*/\\1'\''OpenWrt $(sed -n "s/DISTRIB_DESCRIPTION='\''OpenWrt \\([^ ]*\\) .*/\\1/p" /etc/openwrt_release)'\'',/" /etc/openwrt_release\nsource /etc/openwrt_release \&\& sed -i -e "s/distversion\\s=\\s\\".*\\"/distversion = \\"$DISTRIB_ID $DISTRIB_RELEASE ($DISTRIB_REVISION)\\"/g" -e '\''s/distname    = .*$/distname    = ""/g'\'' /usr/lib/lua/luci/version.lua\nsed -i "s/luciname    = \\".*\\"/luciname    = \\"LuCI openwrt-23.05\\"/g" /usr/lib/lua/luci/version.lua\nsed -i "s/luciversion = \\".*\\"/luciversion = \\"v'$(date +%Y%m%d)'\\"/g" /usr/lib/lua/luci/version.lua\necho "export const revision = '\''v'$(date +%Y%m%d)'\'\'', branch = '\''LuCI openwrt-23.05'\'';" > /usr/share/ucode/luci/version.uc\n/etc/init.d/rpcd restart\n' package/new/default-settings/default/zzz-default-settings
@@ -34,10 +35,12 @@ mv ../master/lite-23.05/haproxy feeds/packages/net/haproxy
 sed -i '/ADDON+=USE_QUIC_OPENSSL_COMPAT=1/d' feeds/packages/net/haproxy/Makefile
 
 # mihomo - prebuilt
-if curl -s "https://$mirror/openwrt/23-config-common-$cfg_ver" | grep -q "^CONFIG_PACKAGE_luci-app-nikki=y" && [ "$MINIMAL_BUILD" = "y" ] && [ "$NO_APPS" != "y" ]; then
+if curl -s "https://$mirror/openwrt/23-config-common-$cfg_ver" | grep -q "^CONFIG_PACKAGE_luci-app-nikki=y" && [ "$NO_APPS" != "y" ]; then
     mkdir -p files/etc/nikki/run
-    curl -Lso files/etc/nikki/run/GeoSite.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
-    curl -Lso files/etc/nikki/run/GeoIP.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
+    if [ "$MINIMAL_BUILD" = "y" ]; then
+        curl -Lso files/etc/nikki/run/GeoSite.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+        curl -Lso files/etc/nikki/run/GeoIP.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
+    fi
     curl -Lso files/etc/nikki/run/geoip.metadb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb
     curl -Lso files/etc/nikki/run/ASN.mmdb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb
     curl -Lso dist.zip https://$github/Zephyruso/zashboard/releases/latest/download/dist-cdn-fonts.zip
